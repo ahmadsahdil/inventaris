2f5be0a0abc4b33f62a4650bc9f00a6210830b45
ahmad <ahmad1510520140@gmail.com>
2021-04-11 16:55:17 +0800

    add database

---
+++ poskobumn.sql
@@ -0,0 +1,292 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql-server
+-- Waktu pembuatan: 11 Apr 2021 pada 08.54
+-- Versi server: 8.0.23
+-- Versi PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poskobumn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int NOT NULL,
+  `nama_barang` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `stok` int NOT NULL,
+  `satuan` varchar(20) DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `total_masuk` int NOT NULL,
+  `total_keluar` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `stok`, `satuan`, `keterangan`, `total_masuk`, `total_keluar`) VALUES
+(2028, 'ada', 0, 'ada', NULL, 12, 12),
+(2029, 'aduh', 0, 'aduh', NULL, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `id_barang_keluar` int NOT NULL,
+  `id_penerima` int NOT NULL,
+  `id_barang` int NOT NULL,
+  `jumlah_keluar` int NOT NULL,
+  `tgl_keluar` date NOT NULL,
+  `keterangan_keluar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang_keluar`
+--
+
+INSERT INTO `barang_keluar` (`id_barang_keluar`, `id_penerima`, `id_barang`, `jumlah_keluar`, `tgl_keluar`, `keterangan_keluar`) VALUES
+(5, 36, 2028, 10, '2021-04-11', 'ada'),
+(6, 37, 2028, 1, '2021-04-11', 'hgfh'),
+(7, 37, 2028, 1, '2021-04-30', '');
+
+--
+-- Trigger `barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `kurang_stok_keluar` AFTER INSERT ON `barang_keluar` FOR EACH ROW BEGIN UPDATE barang SET stok=stok-new.jumlah_keluar , total_keluar=total_keluar+new.jumlah_keluar
+WHERE id_barang=new.id_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tambah_stok_keluar` AFTER DELETE ON `barang_keluar` FOR EACH ROW BEGIN UPDATE barang SET stok=stok+old.jumlah_keluar , total_keluar=total_keluar-old.jumlah_keluar
+WHERE id_barang=old.id_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_barang_masuk` int NOT NULL,
+  `id_pemberi` int NOT NULL,
+  `id_barang` int NOT NULL,
+  `jumlah` int NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `keterangan_masuk` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id_barang_masuk`, `id_pemberi`, `id_barang`, `jumlah`, `tgl_masuk`, `keterangan_masuk`) VALUES
+(201, 12, 2028, 12, '2021-04-11', '');
+
+--
+-- Trigger `barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `kurang_stok` AFTER DELETE ON `barang_masuk` FOR EACH ROW BEGIN UPDATE barang SET stok=stok-old.jumlah , total_masuk=total_masuk-old.jumlah
+WHERE id_barang=old.id_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tambah_stok_jenis` AFTER INSERT ON `barang_masuk` FOR EACH ROW BEGIN UPDATE barang SET stok=stok+NEW.jumlah ,total_masuk=total_masuk+NEW.jumlah
+WHERE id_barang=NEW.id_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemberi`
+--
+
+CREATE TABLE `pemberi` (
+  `id_pemberi` int NOT NULL,
+  `nama_pemberi` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pemberi`
+--
+
+INSERT INTO `pemberi` (`id_pemberi`, `nama_pemberi`, `nama`) VALUES
+(12, 'jaya karta', 'ahmad1'),
+(13, 'ada', 'ada');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerima`
+--
+
+CREATE TABLE `penerima` (
+  `id_penerima` int NOT NULL,
+  `nama_penerima` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `penerima`
+--
+
+INSERT INTO `penerima` (`id_penerima`, `nama_penerima`, `alamat`) VALUES
+(36, 'aku', 'akuu'),
+(37, 'pasek', 'adas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` enum('Admin','Operator') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `status`) VALUES
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Admin'),
+(3, 'operator', 'fe96dd39756ac41b74283a9292652d366d73931f', 'Operator');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD PRIMARY KEY (`id_barang_keluar`),
+  ADD KEY `id_bantuan_masuk` (`id_barang`),
+  ADD KEY `id_posko` (`id_penerima`);
+
+--
+-- Indeks untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_barang_masuk`),
+  ADD KEY `id_donatur` (`id_pemberi`),
+  ADD KEY `id_jenis_bantuan` (`id_barang`);
+
+--
+-- Indeks untuk tabel `pemberi`
+--
+ALTER TABLE `pemberi`
+  ADD PRIMARY KEY (`id_pemberi`);
+
+--
+-- Indeks untuk tabel `penerima`
+--
+ALTER TABLE `penerima`
+  ADD PRIMARY KEY (`id_penerima`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2030;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `id_barang_keluar` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id_barang_masuk` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202;
+
+--
+-- AUTO_INCREMENT untuk tabel `pemberi`
+--
+ALTER TABLE `pemberi`
+  MODIFY `id_pemberi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `penerima`
+--
+ALTER TABLE `penerima`
+  MODIFY `id_penerima` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD CONSTRAINT `barang_keluar_ibfk_4` FOREIGN KEY (`id_penerima`) REFERENCES `penerima` (`id_penerima`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_keluar_ibfk_5` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD CONSTRAINT `barang_masuk_ibfk_3` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `barang_masuk_ibfk_4` FOREIGN KEY (`id_pemberi`) REFERENCES `pemberi` (`id_pemberi`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
